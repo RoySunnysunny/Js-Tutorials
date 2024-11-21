4d988156850d91eeffe57479addc284530bc8172
Sunny Roy <146196525+RoySunnysunny@users.noreply.github.com>
2024-11-21 09:30:57 +0000

    datatypes committed

---
+++ basic/datatypes.js
@@ -0,0 +1,30 @@
+//*************** Primitive Type ******************//
+
+// 7 Types == string, number, boolean, null, undefined, Symbol, BigInt
+
+//*************** Reference Type (Non Premitive) ******************//
+
+// Array, Objects, Functions.
+
+const outsideTemp = null
+let userEmail;
+
+const Id = Symbol('125');
+const sumId = Symbol('125')
+
+console.log(Id === sumId);
+
+const bigIntNumber = 12455455454125445554445n
+
+const ManName = ["sunny", "Kaman", "Tiri", "Joprey"];
+
+const DataObj = {
+    name:'sunny',
+    age:27,
+    address:'kolkata'
+}
+
+const myfu = function(){
+    console.log("Hello World");
+    
+}
