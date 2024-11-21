5dc012f3b62cd2555553a3dc6e4c1bc9ddcf9a68
Sunny Roy <146196525+RoySunnysunny@users.noreply.github.com>
2024-11-21 06:09:27 +0000

    convertion file

---
+++ basic/convertion.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let score = "33abc"
+
+//console.log(typeof score);
+
+let convertInNumber = Number(score)
+//console.log(typeof convertInNumber);
+
+// Number Convertion let see 
+// 33 => 33;
+// "33" => Number;
+// "33abc" => NaN
+// True => 1;
+// False => 0;
+
+let isLoggedIn = 0;
+let bologgedIn = Boolean(isLoggedIn);
+
+//console.log(bologgedIn);
+
+// 1=> True, 2=> False, ""=> False, "Sunny"=> True All Boolean Convertion
+
+let numberval = 33
+
+let numberTostring = String(numberval)
+
+console.log(numberTostring);
+console.log(typeof numberTostring);
+
