abb1935ff11e436b92d6d21a2803eb33d10f3883
Sunny Roy <146196525+RoySunnysunny@users.noreply.github.com>
2024-11-22 05:05:45 +0000

    string js

---
+++ basic/string.js
@@ -0,0 +1,19 @@
+let firstName = "sunny"
+let lastName = "Roy"
+
+console.log(`hello My Fisrt Name is ${firstName} and my last name is ${lastName}`);
+
+
+const gameName = new String('sunny roy')
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(0));
+
+// console.log(gameName.length);
+// console.log(gameName.indexOf('Y'));
+
+const anotherGameName = new String('Sujan')
+console.log(anotherGameName.substring(0,4));
+
+// All the string related function....
+
