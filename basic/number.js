dc65b32f9c5f655c276a1d4d9394e168a6c2bc07
Sunny Roy <146196525+RoySunnysunny@users.noreply.github.com>
2024-11-22 06:32:26 +0000

    math changes

---
+++ basic/number.js
@@ -0,0 +1,40 @@
+let score = 400
+
+//console.log(score);
+let balance = new Number(100)
+//console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.896
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000
+
+///console.log(hundred.toLocaleString('en-In'));
+
+//================== Maths =======================//
+
+// console.log(Math.abs(-8));
+// console.log(Math.round(8.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(8.5));
+// console.log(Math.max(1,8,5,6));
+// console.log(Math.min(1,8,5,6));
+
+//console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random()*(max - min +1))+ min);
+
+
+
+
+
+
+
+
