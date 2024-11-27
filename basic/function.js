572b46091f6f13b8f6e531c7d7258c2eee6eb825
Sunny Roy <146196525+RoySunnysunny@users.noreply.github.com>
2024-11-27 06:53:16 +0000

    changes in functions

--- basic/function.js
+++ basic/function.js
@@ -32,5 +32,39 @@ function logusermsg(username = "testuser"){
     return `${username} just logged in`
 }
 
-console.log(logusermsg("sunny"));
+//console.log(logusermsg("sunny"));
 
+// using rest/spread operator (...test) like this 
+function cartPrice(...num1)
+{
+    return num1;
+}
+
+
+//console.log(cartPrice(2,3,4));
+
+const usersData = {
+    id:1,
+    name:'sunny',
+    age:12,
+    location: 'kolkata'
+}
+
+function handleObject(anyObject)
+{
+    console.log(`The user name is ${anyObject.name} and age is ${anyObject.age}`);
+}
+//handleObject(usersData);
+ //========= we can define object after creating function we have check if it is object or not ===//
+ handleObject({
+    name:'johnny',
+    age:27
+ })
+ const myNewArray = ['sunny','johnny','ronny']
+
+ function arrayFunc(allArr)
+ {
+    console.log(`All the name in the Array is ${allArr[0]}`);
+ }
+
+ arrayFunc(myNewArray);
