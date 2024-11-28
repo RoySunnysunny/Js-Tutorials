0140fa59bd8224b100d46c49a25720dd874d6950
Sunny Roy <146196525+RoySunnysunny@users.noreply.github.com>
2024-11-28 05:59:02 +0000

    scope file

--- basic/scope.js
+++ basic/scope.js
@@ -15,4 +15,51 @@ if(true)
 //     const element = array[i];
     
 // }
-console.log("outer value of", a);
+//console.log("outer value of", a);
+
+function parentFunction()
+{
+    const num = 400+200;
+    function childFunction()
+    {
+        const chnum = 200-100;
+        console.log(num);
+        
+    }
+    //console.log(chnum);
+    
+    childFunction()
+
+}
+//parentFunction();
+
+if(true)
+{
+    const uname = "Sunny Roy"
+    if(uname === "Sunny Roy")
+    {
+        const website = "youtube.com"
+        //console.log(uname+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(uname);
+
+//===================== interesting =================//
+console.log(addOne(5));
+
+//===================== In this function you can call this function after and before declaration =================//
+function addOne(num)
+{
+    return num + 1;
+}
+
+
+
+const addTwo = function(num)
+{
+    return num + 2
+}
+console.log(addTwo(6));
